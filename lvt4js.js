4ed0353248dfc3c0e084ce19aa04883aadedf16a
lichenxi <lichenxi@rd.netease.com>
2017-03-03 17:20:35 +0800

    dev

--- lvt4js.js
+++ lvt4js.js
@@ -292,10 +292,6 @@ LVT.obj = {
 if(!Tobj) var Tobj=LVT.obj;
 if(!$obj) var $obj=LVT.obj;
 
-/** 匹配url的正则 */
-LVT.urlRegex = new RegExp('^(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|localhost|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*$');
-if(!TurlRegex) var TurlRegex=LVT.urlRegex;
-
 LVT.dateRegex = /^\d{4}-\d{1,2}-\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}$/;
 if(!TdateRegex) var TdateRegex=LVT.dateRegex;
 
